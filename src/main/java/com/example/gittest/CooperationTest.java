dcd9e1b42b5796863ceb29da0a5355e90e426a35
wudaofen <wudaofen@mail.ynu.edu.cn>
2022-10-27 17:50:49 +0800

    测试

--- src/main/java/com/example/gittest/CooperationTest.java
+++ src/main/java/com/example/gittest/CooperationTest.java
@@ -3,5 +3,6 @@ package com.example.gittest;
 public class CooperationTest {
     public static void main(String[] args) {
         System.out.println("hello! my collaborator");
+        System.out.println("I am wdf,and I am one of friends of zjr");
     }
 }
